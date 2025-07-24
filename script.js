53c5af7aaa4ae844d4e91bce691d0d5709d211db
rrave34 <znginefe@gmail.com>
2025-07-24 13:12:39 +0300

    .

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburgerMenuCloseButton = document.querySelector(".close-icon");
   const mobileMenu = document.querySelector(".hamburger-menu-wrapper");
 
-  // Menü Aç
   hamburgerMenuOpenButton.addEventListener("click", () => {
     mobileMenu.classList.add("open");
   });
 
-  // Menü Kapat
+
   hamburgerMenuCloseButton.addEventListener("click", () => {
     mobileMenu.classList.remove("open");
   });
